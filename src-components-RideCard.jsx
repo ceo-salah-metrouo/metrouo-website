df5feb51645fa750b2b7567864f875ec9b72c89b
ceo-salah-metrouo <ceo.a.salah@gmail.com>
2025-10-03 12:59:04 +0300

    Update src-components-RideCard.jsx

--- src-components-RideCard.jsx
+++ src-components-RideCard.jsx
@@ -15,27 +15,43 @@ export default function RideCard({ ride, onAccept, onBid, isClient }) {
           <input
             type="number"
             value={bidAmount}
-            onChange={e=>setBidAmount(e.target.value)}
+            onChange={e => setBidAmount(e.target.value)}
             placeholder="Your bid (EGP)"
           />
-          <button className="btn small" onClick={()=>{onBid(ride.id, parseInt(bidAmount)); setBidAmount('')}}>Bid</button>
+          <button 
+            className="btn small" 
+            onClick={() => {
+              onBid(ride.id, parseInt(bidAmount))
+              setBidAmount('')
+            }}
+          >
+            Bid
+          </button>
         </div>
       )}
 
       {ride.bids.length > 0 && (
         <ul className="bids">
-          {ride.bids.map(b=>(
+          {ride.bids.map(b => (
             <li key={b.id}>
-              {b.amount} EGP {isClient && ride.status==='open' &&
-                <button className="btn small" onClick={()=>onAccept(ride.id, b.id)}>Accept</button>
-              }
+              {b.amount} EGP{' '}
+              {isClient && ride.status === 'open' && (
+                <button 
+                  className="btn small" 
+                  onClick={() => onAccept(ride.id, b.id)}
+                >
+                  Accept
+                </button>
+              )}
             </li>
           ))}
         </ul>
       )}
 
-      {ride.status==='accepted' && ride.acceptedBid && (
-        <div className="success">Accepted Bid: {ride.acceptedBid.amount} EGP</div>
+      {ride.status === 'accepted' && ride.acceptedBid && (
+        <div className="success">
+          Accepted Bid: {ride.acceptedBid.amount} EGP
+        </div>
       )}
     </div>
   )
